d90e05edd9325d8e5fcbce619a71fc06f879ce90
Fabrice Théoleyre <theoleyre@unistra.fr>
2021-03-16 14:21:54 +0100

    colored, separated global logs
    a file with the colored logs (all)

--- scripts/loggers/req_dataset_examples.sql
+++ scripts/loggers/req_dataset_examples.sql
@@ -17,3 +17,17 @@ AND P.type='DATA'
 AND P2.event='TX'
 AND P2.type='ACK'
 WHERE P.event='TX' AND P.type='DATA' AND P.src='141592cc00000003' AND P2.src IS NULL  AND P.dest!='ffffffffffffffff'
+
+
+
+## count the nb of unacked packets per source, per cell
+SELECT COUNT(P.src), P.src, P.dest, P.slotOffset, *
+FROM pkt P
+LEFT JOIN pkt P2
+ON P.asn = P2.asn
+AND P.event='TX' 
+AND P.type='DATA' 
+AND P2.event='TX'
+AND P2.type='ACK'
+WHERE P.event='TX' AND P.type='DATA' AND P2.src IS NULL  AND P.dest!='ffffffffffffffff'
+GROUP BY P.src, P.slotOffset
